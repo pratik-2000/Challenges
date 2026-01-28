1611add3b080b1de064ae20cb66407c0a111dc70
pratik <pspatil617@gmail.com>
2026-01-28 05:36:14 +0530

    Armstrong Number

--- src/37 ArmstrongNumber.java
+++ src/37 ArmstrongNumber.java
@@ -1,16 +1,46 @@
 import java.util.Scanner;
 
 class ArmstrongNumber {
-    public static void main(String [] args)
-    {
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number");
-        int num=scanner.nextInt();
+        int num = scanner.nextInt();
+        System.out.println(isArmstrong(num));
 
     }
-    public static int noDigits(int num)
-    {
 
+    public static boolean isArmstrong(int num) {
+        int digit = noOfDigits(num);
+        int numCopy = num;
+        int finalNum = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            num /= 10;
+            finalNum += power(lastDigit, digit);
+        }
+        return finalNum == numCopy;
+
+    }
+
+    public static int power(int num, int power) {
+
+        int result = 1;
+        int i = 0;
+        while (i < power) {
+            result *= num;
+            i++;
+        }
+        return result;
+    }
+
+    public static int noOfDigits(int num) {
+        int digit = 0;
+        while (num > 0) {
+            digit++;
+            num /= 10;
+
+        }
+        return digit;
     }
 
 }
